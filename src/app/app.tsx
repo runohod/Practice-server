bcf1f0c85fa83ff4c44da2b6632ac294b4c4514a
runohod <andrei.rumynthev1728@yandex.ru>
2026-09-03 22:25:21 +0300

    +

---
+++ src/app/app.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-neutral-900">
+      <h1 className="text-3xl font-bold text-red-500">Rick and Morty</h1>
+    </div>
+  );
+};
