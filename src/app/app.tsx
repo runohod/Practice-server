22a5c6aa01de7541e62d08cde3f2e49caa179983
runohod <andrei.rumynthev1728@yandex.ru>
2026-09-04 20:55:20 +0300

    +

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,5 @@
+import { HomePage } from '@/pages/homePage';
+
 export const App = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-neutral-900">
-      <h1 className="text-3xl font-bold text-red-500">Rick and Morty</h1>
-    </div>
-  );
+  return <HomePage />;
 };
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import { App } from './app.tsx'
+import '@/app/styles/index.css'
+import { App } from '@/app/app'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
