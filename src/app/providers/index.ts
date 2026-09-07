6101526b7bf2e25ebf7779af87e3e1e85f81e89e
runohod <andrei.rumynthev1728@yandex.ru>
2026-09-07 22:02:15 +0300

    +

---
+++ src/app/providers/index.ts
@@ -0,0 +1,3 @@
+export (QueryProvider) from './queryProvider.tsx';
+
+
---
+++ src/app/providers/queryProvider.tsx
@@ -0,0 +1,8 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // QueryClient главное хранилище (кэш) TanStack Query. QueryClientProvider — это React-провайдер (компонент-обёртка).
+import type { ReactNode } from 'react';
+
+
+
+
+
+
